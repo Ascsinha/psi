2a619b4ffa35eac9eb913517131ccabf55a0c928
Acsa Mirella da Silva Dantas <20231041110004@ifrn.local>
2025-07-10 17:57:42 -0300

    Mais JavaScript básico

--- JS/script.js
+++ JS/script.js
@@ -41,3 +41,83 @@ let pessoa = {
 }
 
 console.log(pessoa.nacionalidade)
+
+/* Strings podem ser trabalhadas de três formas diferentes, com aspas duplas, simples e templates strings.*/
+
+/* const idade = 10
+
+if(idade < 18) {
+    console.log('Você não pode ter acesso.')
+}
+else if(idade >= 18 && idade < 25) {
+    console.log('Você pode entrar com ressalvas...')
+}
+else {
+    console.log('Você pode ter acesso.')
+} */
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+array = [10, 'Tiago', true, 'Barreto'] // Listas no Python
+console.log(array)
+
+objetos = {nome: "Acsa", idade: 17, sexo: "F"}
+
+console.log(objetos['nome']) // Entre colchetes ou com ponto; as duas maneiras funcionam.
+
+const usuarios = [{
+    nome: 'Acsa',
+    idade: 17,
+    sexo: 'F',
+    endereco: {
+        rua: 'Rua Lalala',
+        cidade: 'Natal',
+        numero: 43
+    }
+}, {
+    nome: 'Fernando',
+    idade: 23,
+    sexo: 'M',
+    endereco: {
+        rua: 'Rua Lelele',
+        cidade: 'Campinas',
+        numero: 12
+    }
+}
+]
+
+console.log(usuarios[0].endereco.rua)
+console.log(usuarios[1].nome)
+
+const corUsuario = null
+const corPadrao = corUsuario || 'padrao'
+
+console.log(corPadrao)
+
+//Se o usuário não tiver colocado nome, o nome padrão dele será user111. Se ele colocar o nome, apresenta o nome.
+
+const nomeUsuario = ''
+const nomePadrao = nomeUsuario || 'user111'
+
+console.log(nomePadrao)
+
+function soma(valor1, valor2) {
+    return valor1 + valor2
+}
+
+console.log(soma(10, 15))
+
+function somaTodosValores(valores) {
+    let soma = 0
+    for(let i=0; i < valores.length; i++) {
+        if (typeof(valores[i]) !== 'number') {
+            return `Valor inválido`
+        }
+        soma += valores[1]
+    }
+    return soma
+}
+
+console.log(somaTodosValores(10, 20, 30, 40))
