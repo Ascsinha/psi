0cbac63916460bebe1ea2b1c236189eb72ccbadb
Acsa Mirella da Silva Dantas <20231041110004@ifrn.local>
2025-09-16 17:39:55 -0300

    Criando novas rotas e links entre arquivos HTML

--- Python/aulas_flask/app.py
+++ Python/aulas_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ def index():
 @app.route('/contato')
 def contato():
     return 'mirella.a@escolar.ifrn.edu.br'
+    
+@app.route('/exemplo')
+def exemplo():
+    return render_template('exemplo.html')
+
+@app.route('/exemplo2')
+def exemplo2():
+    return render_template('exemplo2.html')
 
 if __name__ == '__main__':
     app.run()
